7db0bb6a3abd41ef43d2c5cec13fd04ea8e2679d
stefan.balea <stefan.balea@axsys.ro>
2022-12-15 10:02:35 +0200

    added keplr for mainnet

--- src/App.js
+++ src/App.js
@@ -3,27 +3,28 @@ import List from "./components/List";
 const BZE_PREFIX = "bze";
 const BZE_CG_ID = "bzedge";
 
-const TESTNET_COIN_DENOM = "testbze";
-const TESTNET_COIN_MIN_DENOM = "utestbze";
+const TESTNET_COIN_DENOM = "tbz";
+const TESTNET_COIN_MIN_DENOM = "utbz";
+const BZE_PREFIX_TESTNET = "testbz";
 
-const RPC_URL = "http://testnet-rpc.getbze.com:26657";
+const RPC_URL = "https://testnet-rpc.getbze.com";
 const REST_URL = "https://testnet.getbze.com";
 
-const BZE_TESTNET_1_SUGGEST_CHAIN = {
-  chainId: "bzetestnet-1",
-  chainName: "BZE Testnet 1",
+const BZE_TESTNET_2_SUGGEST_CHAIN = {
+  chainId: "bzetestnet-2",
+  chainName: "BZE Testnet 2",
   rpc: RPC_URL,
   rest: REST_URL,
   bip44: {
     coinType: 118,
   },
   bech32Config: {
-    bech32PrefixAccAddr: BZE_PREFIX,
-    bech32PrefixAccPub: BZE_PREFIX + "pub",
-    bech32PrefixValAddr: BZE_PREFIX + "valoper",
-    bech32PrefixValPub: BZE_PREFIX + "valoperpub",
-    bech32PrefixConsAddr: BZE_PREFIX + "valcons",
-    bech32PrefixConsPub: BZE_PREFIX + "valconspub",
+    bech32PrefixAccAddr: BZE_PREFIX_TESTNET,
+    bech32PrefixAccPub: BZE_PREFIX_TESTNET + "pub",
+    bech32PrefixValAddr: BZE_PREFIX_TESTNET + "valoper",
+    bech32PrefixValPub: BZE_PREFIX_TESTNET + "valoperpub",
+    bech32PrefixConsAddr: BZE_PREFIX_TESTNET + "valcons",
+    bech32PrefixConsPub: BZE_PREFIX_TESTNET + "valconspub",
   },
   currencies: [
     {
@@ -55,11 +56,62 @@ const BZE_TESTNET_1_SUGGEST_CHAIN = {
   },
 };
 
+const BZE_MAINNET_1_SUGGEST_CHAIN = {
+  chainId: "beezee-1",
+  chainName: "BZE Mainnet",
+  rpc: "https://rpc.getbze.com",
+  rest: "https://rest.getbze.com",
+  bip44: {
+    coinType: 118,
+  },
+  bech32Config: {
+    bech32PrefixAccAddr: BZE_PREFIX,
+    bech32PrefixAccPub: BZE_PREFIX + "pub",
+    bech32PrefixValAddr: BZE_PREFIX + "valoper",
+    bech32PrefixValPub: BZE_PREFIX + "valoperpub",
+    bech32PrefixConsAddr: BZE_PREFIX + "valcons",
+    bech32PrefixConsPub: BZE_PREFIX + "valconspub",
+  },
+  currencies: [
+    {
+      coinDenom: "bze",
+      coinMinimalDenom: "ubze",
+      coinDecimals: 6,
+      coinGeckoId: BZE_CG_ID,
+    },
+  ],
+  feeCurrencies: [
+    {
+      coinDenom: "bze",
+      coinMinimalDenom: "ubze",
+      coinDecimals: 6,
+      coinGeckoId: BZE_CG_ID,
+    },
+  ],
+  stakeCurrency: {
+    coinDenom: "bze",
+    coinMinimalDenom: "ubze",
+    coinDecimals: 6,
+    coinGeckoId: BZE_CG_ID,
+  },
+  coinType: 118,
+  gasPriceStep: {
+    low: 0.01,
+    average: 0.1,
+    high: 1,
+  },
+};
+
 const ITEMS = [
   {
-    id: BZE_TESTNET_1_SUGGEST_CHAIN.chainId,
-    title: BZE_TESTNET_1_SUGGEST_CHAIN.chainName,
-    networkDetails: BZE_TESTNET_1_SUGGEST_CHAIN
+    id: BZE_MAINNET_1_SUGGEST_CHAIN.chainId,
+    title: BZE_MAINNET_1_SUGGEST_CHAIN.chainName,
+    networkDetails: BZE_MAINNET_1_SUGGEST_CHAIN
+  },
+  {
+    id: BZE_TESTNET_2_SUGGEST_CHAIN.chainId,
+    title: BZE_TESTNET_2_SUGGEST_CHAIN.chainName,
+    networkDetails: BZE_TESTNET_2_SUGGEST_CHAIN
   }
 ];
 
